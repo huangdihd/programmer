b9ae6e9f99e806ea64691148f058f8dc2647f284
huangdihd <hd20100104@163.com>
2026-07-08 01:14:15 +0800

    Add `Serialize` to `ProgrammerConfig` and implement default configuration
    This commit adds the `Serialize` trait to `ProgrammerConfig` and implements a default configuration. It also updates the main function to create and manage a configuration directory and file, ensuring that a default configuration is written if it does not exist.

--- src/config/programmer_config.rs
+++ src/config/programmer_config.rs
@@ -1,14 +1,25 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(default)]
 pub struct ProgrammerConfig {
     pub model: String,
     pub base_url: String,
     pub api_key: String,
 }
 
+impl Default for ProgrammerConfig {
+    fn default() -> Self {
+        ProgrammerConfig {
+            model: "Type your model here".to_string(),
+            base_url: "Type your base_url here".to_string(),
+            api_key: "Type your api_key here".to_string(),
+        }
+    }
+}
+
 impl ProgrammerConfig {
-    fn new(model: &str, base_url: &str, api_key: &str) -> ProgrammerConfig {
+    fn new(model: &str, base_url: &str, api_key: &str) -> Self {
         ProgrammerConfig {
             model: model.to_string(),
             base_url: base_url.to_string(),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use ::config::Config;
 use ::config::File;
 use ::config::Environment;
@@ -8,16 +9,29 @@ mod ui;
 pub mod config;
 pub mod app;
 
+const CONFIG_PATH: &str = "";
+
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    let config_dir = dirs::config_dir().unwrap();
+    let programmer_dir = config_dir.join("programmer");
+    if !Path::new(programmer_dir.as_path()).exists() {
+        std::fs::create_dir(&programmer_dir)?;
+    }
+    let config_path = programmer_dir.join("config.toml");
     color_eyre::install()?;
     let terminal = ratatui::init();
     let programmer_config = Config::builder()
-        .add_source(File::with_name("config"))
+        .add_source(File::with_name(config_path.as_path().to_str().unwrap()).required(false))
         .add_source(Environment::with_prefix("Programmer"))
-        .build()?;
+        .build()
+        .unwrap_or_default();
 
     let programmer_config: ProgrammerConfig  = programmer_config.try_deserialize()?;
+
+    if !Path::new(config_path.as_path()).exists() {
+        std::fs::write(config_path, toml::to_string(&programmer_config)?)?;
+    }
 
     let result = App::new(programmer_config).await.run(terminal).await;
     ratatui::restore();
